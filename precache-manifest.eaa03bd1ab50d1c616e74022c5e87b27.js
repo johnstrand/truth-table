c33990898a91c4fa84678a61198782e03c1a8adf
John Strand <john.strand@dekiru.se>
2020-10-24 17:35:22 +0200

    Updates

---
+++ precache-manifest.eaa03bd1ab50d1c616e74022c5e87b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a11f2abb1c1f698f40af4e1327bc092",
+    "url": "/truth-table/index.html"
+  },
+  {
+    "revision": "4784caf6c3235c569ba4",
+    "url": "/truth-table/static/css/main.1c8cc3f7.chunk.css"
+  },
+  {
+    "revision": "de8090b9378a134ed3f5",
+    "url": "/truth-table/static/js/2.bc2ea6a1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/truth-table/static/js/2.bc2ea6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4784caf6c3235c569ba4",
+    "url": "/truth-table/static/js/main.4ff4eb8b.chunk.js"
+  },
+  {
+    "revision": "762ebd2ead8a2e216f07",
+    "url": "/truth-table/static/js/runtime-main.82ad3eff.js"
+  }
+]);
